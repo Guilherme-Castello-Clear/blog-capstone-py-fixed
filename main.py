d5de47f22bd01154c5587502394c0b495ed574fb
Guilherme-Castello-Clear <guilherme.castello.clear@gmail.com>
2023-11-11 16:15:30 -0300

    Feat: Sending contact to email

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+import smtplib
+
+from flask import Flask, render_template, request
 import requests
 from post import Post
 
@@ -6,6 +8,9 @@ app = Flask(__name__)
 
 posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
 post_objects = []
+
+OWN_EMAIL = "pythonmailtest2023@gmail.com"
+OWN_PASSWORD = "ufij jsta grav yrjj"
 
 for post in posts:
     post_obj = Post(post["id"], post["title"], post["subtitle"], post["body"])
@@ -25,6 +30,21 @@ def about():
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+
+
+@app.route("/form-entry", methods=["POST"])
+def receive_data():
+    data = request.form
+    name = data["name"]
+    email = data["email"]
+    phone = data["phone"]
+    message = data["message"]
+    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(OWN_EMAIL, OWN_PASSWORD)
+        connection.sendmail(OWN_EMAIL, OWN_EMAIL, email_message)
+    return "<h1>Successfully sent your message</h1>"
 
 
 @app.route("/post/<int:index>")
